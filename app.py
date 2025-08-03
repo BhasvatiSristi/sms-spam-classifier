dd1c488faa2414e82c6e1b23440d184004c27a96
Bhasvati Sristi <bhasvati2106@gmail.com>
2025-08-03 10:09:30 +0530

    first

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+
+
+ps = PorterStemmer()
+
+# Re-define the transforming function here directly
+def transforming(message):
+    message = message.lower()
+    message = nltk.word_tokenize(message)
+
+    y = []
+    for i in message:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+# Load vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("SMS Spam Classifier")
+
+mesg = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    if mesg:
+        transformed_mesg = transforming(mesg)
+        vector_mesg = tfidf.transform([transformed_mesg])
+        pred = model.predict(vector_mesg)[0]
+
+        if pred == 1:
+            st.error("Spam Message Detected!")
+        else:
+            st.success("This is a Not Spam (Ham) Message.")
+
+st.markdown("""
+    <style>
+        .footer {
+            position: fixed;
+            bottom: 10px;
+            right: 10px;
+            color: #aaa;
+            font-size: 13px;
+        }
+    </style>
+    <div class="footer">
+        Made by <b>Bhasvati Sristi</b> @ IIITDM Kancheepuram
+    </div>
+""", unsafe_allow_html=True)
